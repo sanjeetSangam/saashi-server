271cf620588c64ccaa097be7816b60b37fcecc19
sanjeetSangam <factsbe0@gmail.com>
2022-05-27 00:44:03 +0530

    updated the socket.io for realtime chat features

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const messageRoute = require("./src/routes/messagesRoutes");
 const chatRoutes = require("./src/routes/chatRoutes");
 
 const app = express();
-const socket = require("socket.io");
+
 require("dotenv").config();
 
 // middleware
@@ -42,21 +42,40 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server has started on port : ${process.env.PORT}`);
 });
 
-const io = socket(server, {
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
   cors: {
     origin: "*",
     credentials: true,
   },
 });
 
-global.onlineUsers = new Map();
-
 io.on("connection", (socket) => {
-  socket.on("join_chat", (data) => {
-    socket.join(data);
+  console.log("connnected to socket");
+
+  socket.on("setup_user", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
   });
 
-  socket.on("send_msg", (data) => {
-    socket.to(data.chat._id).emit("recieve", data);
+  socket.on("join_chat", (chat) => {
+    socket.join(chat);
+    console.log("user has joined the chat" + chat);
+  });
+
+  socket.on("send_msg", (newMessage) => {
+    var chat = newMessage.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessage.sender._id) return;
+      socket.in(user._id).emit("recieve", newMessage);
+    });
+  });
+
+  socket.off("setup_user", () => {
+    conosle.log("User has been disconnected");
+    socket.leave(userData._id);
   });
 });
